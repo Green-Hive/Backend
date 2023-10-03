9465d596b3948419acb3766bf27e2f228c1bb4ee
Clément LOISEAU <clement.loiseau@epitech.eu>
2023-10-04 01:01:26 +0200

    change start script prisma

--- src/server.ts
+++ src/server.ts
@@ -7,12 +7,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('Bienvenue sur mon serveur Express !');
+  res.send('Welcome to Green Hive server !');
 });
 
-app.get('/', (req, res) => {
-  res.send('Bienvenue sur mon serveur Express !');
-});
+app.get('/posts', async (req, res) => {});
+
+app.post('/posts', async (req, res) => {});
+
+app.get('/posts/:id', async (req, res) => {});
+
+app.patch('/posts/:id', async (req, res) => {});
+
+app.delete('/posts/:id', async (req, res) => {});
 
 app.listen(process.env.PORT, () => {
   console.log(`====================================================`);
