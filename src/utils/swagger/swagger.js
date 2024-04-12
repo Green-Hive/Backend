fbad6241b7208accf1082010fd9277b6476670d7
Clément L <clement.loiseau@epitech.eu>
2024-04-12 12:43:29 +0200

    puhs

--- src/utils/swagger/swagger.js
+++ src/utils/swagger/swagger.js
@@ -5,8 +5,8 @@ const doc = {
         title: 'Green Hive API',
         description: 'Description'
     },
-    host: 'backend-production-3c19.up.railway.app', // Use the host from DATABASE_URL
-    schemes: ['http', 'https'], // Support both HTTP and HTTPS
+    host: '0.0.0.0:4000',
+    schemes: ['http, https'],
 };
 
 const outputFile = './swagger.json';
