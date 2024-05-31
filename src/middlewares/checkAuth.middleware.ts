af93128441bc91a42d4945ab40fd491b79edcde5
Clément L <clement.loiseau@epitech.eu>
2024-05-31 16:03:37 +0200

    push

--- src/middlewares/checkAuth.middleware.ts
+++ src/middlewares/checkAuth.middleware.ts
@@ -1,7 +1,11 @@
 import {NextFunction, Request, Response} from 'express';
 
 function checkAuthentication(req: Request, res: Response, next: NextFunction) {
-  if ((req.session && req.session.userId) || req.headers.authorization) {
+  if (req.session && req.session.userId) {
+    next();
+  } else if (req.headers.authorization) {
+    console.log('Authorization header found');
+    console.log(req.headers.authorization);
     next();
   } else {
     res.status(401).json({message: 'Unauthorized - User not logged in'});
