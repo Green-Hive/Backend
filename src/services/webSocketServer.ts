5dea6ddd6b4d7e00b30329d22664a1f8b22cad20
Clément L <clement.loiseau@epitech.eu>
2024-06-03 00:51:12 +0200

    push

--- src/services/webSocketServer.ts
+++ src/services/webSocketServer.ts
@@ -5,8 +5,14 @@ const wss = new WebSocketServer({port: Number(process.env.WS_PORT)});
 wss.on('connection', (ws) => {
   console.log('WebSocket client connected');
 
+  ws.on('message', (message) => {
+    console.log(`Received message: ${message}`);
+    // Répondre au client
+    ws.send('Hello from server!');
+  });
+
   ws.on('close', () => {
-    console.log('WebSocket client connected');
+    console.log('WebSocket client disconnected');
   });
 });
 
